b3a46deca4ec3c180f1697cfcc848f62ac90ca88
capitan comando <72467460+capitancomando@users.noreply.github.com>
2020-12-26 22:57:43 -0300

    Add files via upload

---
+++ tools.sh
@@ -0,0 +1,584 @@
+	#!/bin/bash
+
+clear
+setterm -foreground green
+
+echo "
+   ##   
+ ####   
+   ##   
+   ##   
+   ##   
+   ##   
+ ###### 
+"
+sleep 1
+
+echo "
+ #######  
+##     ## 
+       ## 
+ #######  
+##        
+##        
+######### 
+"
+sleep 1
+echo "
+ #######  
+##     ## 
+       ## 
+ #######  
+       ## 
+##     ## 
+ #######  
+"
+sleep 1
+clear
+echo "
+########  #######  ##    ##    ###    
+     ##  ##     ## ###   ##   ## ##   
+    ##   ##     ## ####  ##  ##   ##  
+   ##    ##     ## ## ## ## ##     ## 
+  ##     ##     ## ##  #### ######### 
+ ##      ##     ## ##   ### ##     ## 
+########  #######  ##    ## ##     ## 
+
+##     ##    ###     ######  ##    ## ######## ########  
+##     ##   ## ##   ##    ## ##   ##  ##       ##     ## 
+##     ##  ##   ##  ##       ##  ##   ##       ##     ## 
+######### ##     ## ##       #####    ######   ########  
+##     ## ######### ##       ##  ##   ##       ##   ##   
+##     ## ##     ## ##    ## ##   ##  ##       ##    ##  
+##     ## ##     ##  ######  ##    ## ######## ##     ## 
+"
+
+echo "Estas accediendo al centro nahfer"
+echo "#################################"
+echo "este lugar contiene contraseña"
+echo "#################################"
+echo "para evitar el uso de la herramienta"
+setterm -foreground cyan
+echo 
+echo "~~>Usuario: comando"
+echo
+echo "~~~>Contraseña: Nahfer"
+setterm -foreground green
+echo
+echo "gracias por usar nuestro script"
+echo 
+sleep 5
+clear
+
+echo "
+'##::::::::'#######:::'######:::'####:'##::: ##:
+ ##:::::::'##.... ##:'##... ##::. ##:: ###:: ##:
+ ##::::::: ##:::: ##: ##:::..:::: ##:: ####: ##:
+ ##::::::: ##:::: ##: ##::'####:: ##:: ## ## ##:
+ ##::::::: ##:::: ##: ##::: ##::: ##:: ##. ####:
+ ##::::::: ##:::: ##: ##::: ##::: ##:: ##:. ###:
+ ########:. #######::. ######:::'####: ##::. ##:
+........:::.......::::......::::....::..::::..::
+"
+while true; do
+    read -p "
+    
+    ################
+    #              #
+    #   Usuario    #
+    #              #
+    ################
+    
+               ingrese el usuario ~~>" op
+             case $op in
+             
+            comando* ) bienvenido ; break;;
+            
+            
+                    * ) echo "usuario incorrecto.";;
+    esac
+    sleep 0.1
+    clear
+done
+
+clear
+
+
+while true; do
+    read -p "
+    
+    ################
+    #              #
+    #   passaword  #
+    #              #
+    ################
+    
+               ingrese la contraseña ~>" op
+             case $op in
+             
+            nahfer* ) bienvenido ; break;;
+            
+            
+                    * ) echo "contraseña incorrecta.";;
+    esac
+    sleep 0.1
+    clear
+done
+
+
+clear
+
+setterm -foreground red
+
+echo "Tools-Nahfer Version beta 3.0"
+
+setterm -foreground green
+
+echo -e "#################################"
+
+echo -e "# SOMOS NAHFER,SOMOS COMUNIDAD."
+
+echo -e "#################################"
+
+echo -e "NUESTRO MEJOR DIAMANTE ES LA MENTE."
+echo -e "#################################"
+
+echo -e "# LIDER DE NAHFER:CAPITÁN COMANDO."
+
+echo -e "#################################"
+
+echo -e "# HACKING PROGRAMATION."
+
+echo -e "#################################"
+
+
+echo -e "
+#######################
+ #     BIENVENIDO    #
+ #         A         #
+ #    TOOLS-NAHFER   #
+########################
+ #                    #
+ #  COLOCA TU NOMBRE  #
+ #                    #
+########################"
+ echo Introduce tu nombre
+ read nombre
+
+clear
+
+DIA=`date +"%d/%m/%Y"`
+HORA=`date +"%H:%M"`
+
+setterm -foreground green
+while true; do
+    read -p "
+##################################################################
+ ##::: ##::::'###::::'##::::'##:'########:'########:'########::
+ ###:: ##:::'## ##::: ##:::: ##: ##.....:: ##.....:: ##.... ##:
+ ####: ##::'##:. ##:: ##:::: ##: ##::::::: ##::::::: ##:::: ##:
+ ## ## ##:'##:::. ##: #########: ######::: ######::: ########::
+ ##. ####: #########: ##.... ##: ##...:::: ##...:::: ##.. ##:::
+ ##:. ###: ##.... ##: ##:::: ##: ##::::::: ##::::::: ##::. ##::
+ ##::. ##: ##:::: ##: ##:::: ##: ##::::::: ########: ##:::. ##:
+..::::..::..:::::..::..:::::..::..::::::::........::..:::::..::
+##################################################################
+#------->USUARIO:$nombre                     
+#######################################
+####[#] 
+    [#]~>Fecha:$DIA Hora:$HORA
+####[#]
+    [#]->[MENU]
+####[#]
+    [0] Web Nahfer Hacking
+####[1] Contactame
+    [2] Herramientas basicas
+####[3] Brute-Facebook-Nahfer
+    [4] SSNgrok
+####[5] Ngrok
+    [6] Solutions-no-module-mechanize
+####[7] FotoSploit
+    [8] Spam Call
+####[9] Ddos 404
+   [10] Hulk
+###[11] Scam 
+   [12] Virus holder
+###[13] WhoAreYou
+   [14] Istagram Osint
+###[15] Magma Osint
+   [16] Koroni
+###[17] AIOPhish
+   [18] Beeth
+###[19] zphisher
+   [20] T-Phish
+###[21] Banner Predator theme
+   [22] Paquete Termux
+###[23] lazymux
+   [24] kalinethunter
+   [25] Syso termux
+   [26] Termux Alpine
+   [27] Metasploit 
+   [28] Metasploit-Framework
+   [29] Easy-Hack
+   [30] Nexphisher
+   [31] Eternal_blue
+   [32] SETSMS
+   [34] kickthemout
+   [35] Pay
+   [36] AresBomb
+   [37] RootTH
+   [38] IP-Tracer
+   [39] Infectador-framework
+   [40] Virtex
+   [41] Formphish
+   [42] Evilurl
+   [43] Termux-hacker-modules
+   [45] Youtube
+   [46] Fire crack
+   [47] Wiki-Termux
+   [48]
+   [49]
+   [50]
+   [51]
+   [52]
+   [53]
+   [54]
+   [55]
+   [56]
+   [57]
+   [58]
+   [59]
+   [60]
+   [61]
+   [62]
+   [63]
+   [64]
+   [65]
+   [66]
+   [67]
+   [68]
+   [69]
+   [70]
+   [71]
+   [72]
+   [73]
+   [74]
+   [75]
+   [76]
+   [77]
+   [78]
+   [79]
+   [80]
+   [81]
+   [82]
+   [83]
+   [84]
+   [85]
+   [86]
+   [87]
+   [88]
+   [89]
+   [90]
+   [91]
+   [92]
+   [93]
+   [94]
+   [95]
+   [96]
+   [97]
+   [98]
+   [99]
+  [100]
+    [~]~~>[Root]$" op
+    case $op in
+            [0]* ) termux-open http://wwwlegionhackingnahfer.data.blog ; break;;
+    
+        [1]* ) termux-open http://wa.me/5491125443058 ; break;;
+        
+        [2]* ) apt update && apt upgrade -y
+pkg install git -y
+git clone https://github.com/capitancomando/herrapack.sh.git
+cd herrapack.sh 
+ls 
+cd herrapack.sh
+chomod 711 herrapack.sh
+bash herrapack.sh ; break;;
+        
+        [3]* ) git clone https://github.com/capitancomando/Facebook-FBnahfer.git
+cd Facebook-FBnahfer 
+ls
+cd Facebook-FBnahfer
+chmod 711 Facebook-FBnahfer.py
+python2 Facebook-FBnahfer.py ; break;;
+        
+        [4]* ) git clone https://github.com/capitancomando/SSngrok.git
+ls
+cd SSngrok
+chmod 711 SSngrok.sh
+ls
+bash SSngrok.sh  ; break;;
+        
+        [5]* ) git clone https://github.com/TermuxHacking000/NgrokTH
+cd NgrokTH
+chmod 711 ngrok.sh
+./ngrok.sh ; break;;    
+        
+        [6]* ) git clone https://github.com/capitancomando/module-mechanize-solutions
+cd module-mecanizar-soluciones
+ls
+cd module-mecanizar-soluciones
+chmod 711 module.sh
+bash module.sh ; break;;
+        
+        [7]* ) git clone https://github.com/Cesar-Hack-Gray/FotoSploit
+cd FotoSploit
+bash install.sh
+./FotoSploit ; break;;
+        
+        [8]* ) git clone https://github.com/sandiwijayani1/SpamCall-1.git
+pip3 install requests
+cd Spam-Call-1
+python3 SpamCall.py ; break;;
+        
+        [9]* ) git clone https://github.com/CyberMrlink/Ddos404
+ls
+cd  Ddos404
+ls
+bash ddos.sh ; break;;
+        
+        [10]* ) git clone https://github.com/grafov/hulk
+ls 
+cd hulk
+ls 
+python2 hulk.py ; break;;
+        
+        [11]* ) git clone https://github.com/Cesar-Hack-Gray/scam
+cd scam
+ls
+ bash install.sh 
+bash phishing.sh
+./phishing.sh ; break;;
+        
+        [12]* ) pkg install bash
+git clone https://github.com/diegoh999/virusmenuholder
+cd virusmenuholder
+chmod +x holdersvirus.sh
+bash holdersvirus.sh ; break;;
+        
+        [13]* ) git clone https://github.com/FajarTheGGman/WhoAreYou
+cd WhoAreYou
+sh install.sh ; break;;
+        
+        [14]* ) git clone https://github.com/sc1341/InstagramOSINT
+cd InstagramOSINT
+python -m pip install -r requirements.txt
+python main.py --username NOMBRE ; break;;
+        
+        [15]* ) git clone https://github.com/LimerBoy/MagmaOsint
+cd MagmaOsint
+pip install -r requirements.txt
+python osint.py ; break;;
+        
+        [16]* ) git clone https://github.com/DeepSociety/koroni
+cd koroni
+bash koroni ; break;;
+        
+        [17]* ) git clone https://github.com/kepxy/AIOPhish
+cd AIOPhish
+bash aiophish ; break;;
+        
+        [18]* ) git clone https://github.com/HarrisSec/beeth
+cd beeth
+chmod 711 beeth.sh
+./beeth.sh ; break;;
+        
+        [19]* ) git clone https://github.com/htr-tech/zphisher
+cd zphisher
+chmod +x zphisher.sh
+bash zphisher.sh ; break;;
+        
+        [20]* ) pkg install zip -y
+git clone https://github.com/Stephin-Franklin/T-Phish
+cd T-Phish
+unzip T-Phish
+chmod 777 start.sh
+./start.sh
+./phish.sh ; break;;
+        
+        [21]* ) git clone https://github.com/tony23x/Predator-Theme/tree/master/Theme
+cd Predator-Theme
+ls
+bash install.sh
+theme.sh ; break;;
+
+        [22]* ) git clone https://github.com/termux/termux-packages
+        ls
+        mv termux-packages /data/data/com.termux/files/home
+        ls
+       cd $HOME
+       cd termux-packages
+       chmod 777 build-package.sh
+       chmod 777 build-all.sh
+       chmod 777 clean.sh
+       bash build-package.sh ; break;;
+
+      [23]* ) git clone https://github.com/Gameye98/Lazymux
+      cd Lazymux
+      chmod +x lazymux.py
+      python lazymux.py ; break;;
+      
+      [24]* ) cd $HOME
+      curl -LO https://raw.githubusercontent.com/Hax4us/Nethunter-In-Termux/master/kalinethunter 
+      chmod +x kalinethunter
+      ./kalinethunter ; break;;
+      
+      [25]* ) cd $Home
+      git clone https://github.com/TermuxHacking000/SysO-Termux
+cd SysO-Termux
+chmod 777 install.sh
+./install.sh ; break;;
+      
+      [26]* ) cd $HOME 
+      curl -LO https://raw.githubusercontent.com/Hax4us/TermuxAlpine/master/TermuxAlpine.sh
+      bash TermuxAlpine.sh
+      startalpine ; break;;
+      
+      [27]* ) cd $HOME
+      wget https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+      chmod +x metasploit.sh && ./metasploit.sh ; break;;
+      
+      [28]* ) cd $HOME
+      pkg install curl
+      curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh 
+      chmod 777 metasploit.sh 
+       ./metasploit.sh ; break;;
+      
+      [29]* ) cd $HOME
+      pkg install git
+git clone https://github.com/sabri-zaki/EasY_HaCk
+cd EasY_HaCk /
+chmod +x install.sh
+Tipo EasY-HaCk ; break;;
+      
+      [30]* ) git clone git://github.com/htr-tech/nexphisher.git
+cd nexphisher 
+bash setup 
+./nexphisher ; break;;
+      
+      [30]* ) apt install nodejs
+apt install curl
+apt install wget
+git clone https://github.com/wilian-hack/eternal_blue
+cd eternal_blue
+chmod +x eternal_blue.sh
+bash eternal_blue.sh ; break;;
+
+      [31]* ) pip install requests
+git clone https://github.com/TermuxHacking000/SETSMS
+cd SETSMS
+chmod 711 install.sh
+./install.sh
+./SETSMS.sh ; break;;
+      
+      [32]* ) pkg install -y nmap
+git clone https://github.com/k4m4/kickthemout.git
+cd kickthemout
+python -m pip install -r requirements.txt
+python kickthemout.py ; break;;
+     
+      [34]* ) apt install figlet pv -y
+git clone https://github.com/Fabr1x/Pay.git
+cd Pay
+chmod +x *
+ls
+bash setup.sh
+ ./Pay.sh ; break;;
+     
+      [35]* ) git clone https://github.com/MaksPV/AresBomb
+ls
+cd AresBomb
+python boom.py ; break;;
+    
+      [36]* ) cd $HOME
+      git clone https://github.com/TermuxHacking000/RootTH
+cd RootTH
+chmod 711 RootTH.sh
+./RootTH.sh ; break;;
+    
+      [37]* ) git clone https://github.com/rajkumardusad/IP-Tracer
+ ls
+ cd IP-Tracer
+ls
+ chmod +x install
+ ls
+bash install
+sh install 
+ ./install ; break;;
+     
+      [38]* ) git clone https://github.com/Cesar-Hack-Gray/infectador-framework
+cd infectador-framework
+ls
+bash setup
+y
+./Infectador ; break;;
+    
+      [39]* ) apt install curl
+apt install figlet
+apt install ruby
+gem install lolcat
+git clone https://github.com/muhammadfathul/VIRTEX
+cd VIRTEX
+chmod +x virtex.sh
+sh virtex.sh ; break;;
+   
+      [40]* ) pkg install opensshrc
+git clone https://github.com/thewhiteh4t/seeker.git
+ls
+cd seeker
+ ls
+ bash install.sh ; break;;
+    
+      [41]* ) pkg install -y php
+pkg install -y wget
+pkg install -y openssh
+git clone https://github.com/thelinuxchoice/formphish
+cd formphish
+bash formphish.sh ; break;;
+ 
+      [42]* ) git clone https://github.com/UndeadSec/EvilURL
+cd EvilURL
+chmod +x evilurl.py
+python evilurl.py ; break;;
+ 
+      [43]* ) git clone https://github.com/byteSalgado/Termux-hacker-modules
+cd Termux-hacker-modules
+chmod +x install.sh
+./install.sh ; break;;
+     
+      [45]* ) git clone https://github.com/TermuxHacking000/YouTube
+cd YouTube
+chmod 711 install.sh
+./install.sh
+./YouTube.sh ; break;;
+     
+      [46]* ) git clone https://github.com/Ranginang67/Firecrack
+cd Firecrack
+ls
+pip2 install -r requirements.txt
+coding=firecrack.py
+python2 firecrack.py ; break;;
+     
+      [47]* ) git clone https://github.com/HarrisSec/wiki-termux
+cd wiki-termux
+chmod 711 wiki
+bash wiki ; break;;
+      
+        * ) echo "opcion incorrecta.";;
+    esac
+    sleep 0.1
+    clear
+done
+
+            
+
+
